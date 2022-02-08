f80f08ddce6bf523614d88fa8ae59ef66c91baa1
alexmwaura <alexmwaura43@gmail.com>
2022-02-08 13:42:24 +0300

    initial commit

---
+++ src/main/java/com/mwaurascode/demo/student/Student.java
@@ -0,0 +1,10 @@
+package com.mwaurascode.demo.student;
+
+import java.util.UUID;
+
+public class Student {
+        private final UUID studentId;
+        private final String firstName;
+        private  final String lastName;
+
+}
